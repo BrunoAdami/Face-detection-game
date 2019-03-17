1778fbbda5e2d193499cdf7a8112f4a1555d19dd
Bruno Adami <brunoadamiserine@gmail.com>
2019-03-16 21:17:25 -0300

    Updates

---
+++ precache-manifest.8a46093c877bff5c8d6e334a19c71c42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "78c31f886b058e47e6bc",
+    "url": "/Face-recognition-app/static/css/main.d84b0c45.chunk.css"
+  },
+  {
+    "revision": "78c31f886b058e47e6bc",
+    "url": "/Face-recognition-app/static/js/main.f8e41243.chunk.js"
+  },
+  {
+    "revision": "f085ba2f4baf410752f0",
+    "url": "/Face-recognition-app/static/js/runtime~main.117cc877.js"
+  },
+  {
+    "revision": "a196f980b60645c19fe1",
+    "url": "/Face-recognition-app/static/css/2.dec2db43.chunk.css"
+  },
+  {
+    "revision": "a196f980b60645c19fe1",
+    "url": "/Face-recognition-app/static/js/2.e6d836fe.chunk.js"
+  },
+  {
+    "revision": "ab41af34b531635d8831a3cec59d90ed",
+    "url": "/Face-recognition-app/static/media/face.ab41af34.png"
+  },
+  {
+    "revision": "04e24f915ca36237ccad863b3f9b0d41",
+    "url": "/Face-recognition-app/index.html"
+  }
+];
